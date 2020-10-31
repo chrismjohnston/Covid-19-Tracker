55b63365bc4d0232da94f95b384656e24b6da4bc
Christopher Johnston <cmjohnston@gmail.com>
2020-10-31 14:20:07 -0500

    added useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
   MenuItem,
   FormControl,
@@ -7,17 +7,28 @@ import {
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
+
+
+
   return (
     <div className="app">
-      <h1>COVID 19 TRACKER</h1>
-      <FormControl className="app-dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option 1</MenuItem>
-          <MenuItem value="worldwide">Option two</MenuItem>
-          <MenuItem value="worldwide">Option 3</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app_header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 1</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+  
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
